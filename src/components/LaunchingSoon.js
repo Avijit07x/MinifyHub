c2c5bf69d0d49008d2ba5f2706f8423268339408
Avijit07x <deyavijit134@gmail.com>
2024-11-14 19:04:10 +0530

    fix

--- src/components/LaunchingSoon.js
+++ src/components/LaunchingSoon.js
@@ -4,7 +4,7 @@ const LaunchingSoon = () => {
 			<div className="text-center space-y-4">
 				<h1 className="text-4xl font-bold">Launching Soon</h1>
 				<p className="text-lg">
-					We're working hard to bring something amazing to you. Stay tuned!
+					We&#39;re working hard to bring something amazing to you. Stay tuned!
 				</p>
 				<button className="mt-6 px-6 py-2 font-medium text-[#111827] bg-[#B8C5E0] rounded hover:bg-opacity-90">
 					Notify Me
