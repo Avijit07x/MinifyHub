1fddd3f63bdd865861bd9e476c3876b347ce2f18
Avijit07x <deyavijit134@gmail.com>
2024-11-14 19:21:18 +0530

    fix btn

--- src/components/LaunchingSoon.js
+++ src/components/LaunchingSoon.js
@@ -6,7 +6,7 @@ const LaunchingSoon = () => {
 				<p className="text-lg">
 					We&#39;re working hard to bring something amazing to you. Stay tuned!
 				</p>
-				<button className="mt-6 px-6 py-2 font-medium text-[#111827] bg-[#B8C5E0] rounded hover:bg-opacity-90">
+				<button className="mt-6 px-6 py-2 font-medium text-[#111827] bg-[#B8C5E0] rounded-lg hover:bg-opacity-90">
 					Notify Me
 				</button>
 			</div>
